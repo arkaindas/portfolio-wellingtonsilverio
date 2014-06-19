cac7ee50a6bc7bd3cb7bdd9e1822878d084414dd
Wellington A. A. Silverio <wellington.silverio@etec.sp.gov.br>
2014-06-18 21:38:06 -0300

    adicionado reset, padrao, estrutura, topo, perfil e responsive

---
+++ index.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<html>
+<head>
+<title>desenvolvimento desktop, mobile, web &amp; designer - Campinas, SP | Wellington A. A. Silverio.</title>
+<link href='http://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<META HTTP-EQUIV="Expires" CONTENT="0">
+<META HTTP-EQUIV="Cache-Control" CONTENT="no-cache, must-revalidate">
+<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+
+<META NAME="description" CONTENT="portfolio de Wellington A. A. Silverio"> 
+
+<link rel="canonical" href="http://wellingtonsilverio.com"/>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="content-language" content="pt-br" />
+<meta name="viewport" id="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=3.0,initial-scale=1.0" />
+<meta name="msapplication-tap-highlight" content="no" />
+
+<!-- ICON MAC -->  
+<link rel="apple-touch-icon" href="imgs/favicon.png"/>
+<link rel="apple-touch-icon" sizes="72x72" href="himgs/favicon72x.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="imgs/favicon114x.png" />
+<link rel="apple-touch-icon" sizes="129x129" href="imgs/favicon129x.png" />  
+<!-- FAVICON HTML5-->
+<link rel="icon" href="imgs/favicon.png">
+<!-- FAVICON IE-->
+<link rel="icon" type="image/png" href="imgs/favicon.png">
+
+<link href="http://plus.google.com/111438862844007483552" rel="publisher" />
+<!-- CSS - RESET E PRESET -->
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<!-- CSS - RESPONSIVO -->
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+<!-- CSS - PADRAO -->
+<link rel="stylesheet" type="text/css" href="css/padrao.css">
+<!-- CSS - PAGINA HOME -->
+<link rel="stylesheet" type="text/css" href="css/home.css">
+
+
+<meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=3" />
+
+
+<link rel="author" href="http://plus.google.com/111438862844007483552"/>
+
+<link rel="publisher" href="http://plus.google.com/111438862844007483552"/>
+
+</head>
+
+<body>
+<!-- Fazer um carregar div no meio(com a informacao e a porcentagem) e overflow hidden ate carregar -->
+<div id="banner">
+	<header id="perfil">
+    	<div id="foto"></div>
+        <h1>Wellington A. A. Silverio</h1>
+        <p>desenvolvimento desktop, mobile, web &amp; designer</p>
+        <div id="seguir"></div>
+    </header>
+</div>
+<div id="servicos">
+	<h1>Serviços</h1>
+    <div><img src="" title="" /><h2>DESIGN</h2><p></p></div>
+    <div><img src="" title="" /><h2>DEVELOPMENT</h2><p></p></div>
+    <div><img src="" title="" /><h2>CONSULTING</h2><p></p></div>
+</div>
+<div id="empresas">
+	<img src="" title="" />
+    <img src="" title="" />
+    <img src="" title="" />
+    <img src="" title="" />
+</div>
+<div id="perfilCompleto">
+	<div id="fotoVideo"></div>
+	<div id="sobre"><h1></h1><p></p></div>
+	<div id="informacoes">
+    	<section><img src="" title="" /></section>
+    	<section><img src="" title="" /></section>
+    	<section><img src="" title="" /></section>
+    	<section><img src="" title="" /></section>
+    	<section><img src="" title="" /></section>
+    	<section><img src="" title="" /></section>
+    </div>
+</div>
+<div id="projetos">
+	<div>
+        <h1>titulo</h1>
+        <h2>O | QUE | FEZ</h2>
+        <img src="" title="" />
+    </div>
+	<div>
+        <h1>titulo</h1>
+        <h2>O | QUE | FEZ</h2>
+        <img src="" title="" />
+    </div>
+	<div>
+        <h1>titulo</h1>
+        <h2>O | QUE | FEZ</h2>
+        <img src="" title="" />
+    </div>
+	<div>
+        <h1>titulo</h1>
+        <h2>O | QUE | FEZ</h2>
+        <img src="" title="" />
+    </div>
+	<div>
+        <h1>conheça mais</h1>
+        <h2>DIVERSOS</h2>
+        <img src="" title="" />
+    </div>
+</div>
+<div id="opensource">
+	<!-- IDEIA: colocar um debate, sobre Open source.  -->
+    <h1></h1>
+    <p></p>
+    <div id="ultimosOpenSource">
+    	<div></div><div></div><div></div><div></div><div></div>
+    	<div></div><div></div><div></div><div></div><div></div>
+    </div>
+</div>
+</body>
+</html>
