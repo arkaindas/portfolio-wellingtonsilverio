7367d87a21a968eebcc3be82dcb8f091165014c0
Wellington A. A. Silverio <wellington.silverio@etec.sp.gov.br>
2014-06-20 01:06:02 -0300

    criada a arquitetura da pagina, perfil, serviços, projetos...

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html>
 <head>
 <title>desenvolvimento desktop, mobile, web &amp; designer - Campinas, SP | Wellington A. A. Silverio.</title>
-<link href='http://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
-<link href='css/font.css' rel='stylesheet' type='text/css'>
+<link href='css/roboto.css' rel='stylesheet' type='text/css'>
+<link href='css/weloveiconfonts.com.css' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/fontello.css">
+    <link rel="stylesheet" href="css/animation.css"><!--[if IE 7]><link rel="stylesheet" href="css/fontello-ie7.css"><![endif]-->
 
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <META HTTP-EQUIV="Expires" CONTENT="0">
@@ -29,7 +31,7 @@
 <!-- FAVICON IE-->
 <link rel="icon" type="image/png" href="imgs/favicon.png">
 
-<link href="http://plus.google.com/111438862844007483552" rel="publisher" />
+<link href="http://plus.google.com/106416306791358366695" rel="publisher" />
 <!-- CSS - RESET E PRESET -->
 <link rel="stylesheet" type="text/css" href="css/reset.css">
 <!-- CSS - RESPONSIVO -->
@@ -43,9 +45,9 @@
 <meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=3" />
 
 
-<link rel="author" href="http://plus.google.com/111438862844007483552"/>
+<link rel="author" href="http://plus.google.com/106416306791358366695"/>
 
-<link rel="publisher" href="http://plus.google.com/111438862844007483552"/>
+<link rel="publisher" href="http://plus.google.com/106416306791358366695"/>
 
 </head>
 
@@ -53,9 +55,9 @@
 <!-- Fazer um carregar div no meio(com a informacao e a porcentagem) e overflow hidden ate carregar -->
 <div id="banner">
 	<div id="statisticas">
-    	<div id="boxSts"><section><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path fill="#29B99A" d="M25,49.794c-0.947,0-1.854-0.224-2.551-0.625L5.345,39.293c-1.454-0.839-2.551-2.738-2.551-4.419V15.125c0-1.679,1.097-3.578,2.551-4.418l17.104-9.875C23.146,0.428,24.053,0.206,25,0.206s1.854,0.222,2.552,0.625l17.104,9.875c1.453,0.84,2.551,2.74,2.551,4.418v19.749c0,1.681-1.098,3.58-2.551,4.419l-17.104,9.876C26.854,49.57,25.947,49.794,25,49.794zM25,2.492c-0.545,0-1.058,0.116-1.408,0.319L6.488,12.687c-0.737,0.425-1.409,1.587-1.409,2.438v19.749c0,0.852,0.671,2.015,1.409,2.44l17.103,9.875c0.35,0.202,0.863,0.318,1.408,0.318s1.058-0.116,1.407-0.318l17.105-9.875c0.737-0.426,1.408-1.589,1.408-2.44V15.125c0-0.851-0.671-2.014-1.408-2.438L26.408,2.811C26.058,2.608,25.545,2.492,25,2.492z"></path></svg><div id="qntSts">571</div></section><div id="ttlSts">COPOS DE CAFE</div></div>
-        <div id="boxSts"><section><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path fill="#2BBCEE" d="M25,49.794c-0.947,0-1.854-0.224-2.551-0.625L5.345,39.293c-1.454-0.839-2.551-2.738-2.551-4.419V15.125c0-1.679,1.097-3.578,2.551-4.418l17.104-9.875C23.146,0.428,24.053,0.206,25,0.206s1.854,0.222,2.552,0.625l17.104,9.875c1.453,0.84,2.551,2.74,2.551,4.418v19.749c0,1.681-1.098,3.58-2.551,4.419l-17.104,9.876C26.854,49.57,25.947,49.794,25,49.794zM25,2.492c-0.545,0-1.058,0.116-1.408,0.319L6.488,12.687c-0.737,0.425-1.409,1.587-1.409,2.438v19.749c0,0.852,0.671,2.015,1.409,2.44l17.103,9.875c0.35,0.202,0.863,0.318,1.408,0.318s1.058-0.116,1.407-0.318l17.105-9.875c0.737-0.426,1.408-1.589,1.408-2.44V15.125c0-0.851-0.671-2.014-1.408-2.438L26.408,2.811C26.058,2.608,25.545,2.492,25,2.492z"></path></svg><div id="qntSts">0,1</div></section><div id="ttlSts" style="margin-top:70px;">MIL LINHAS PROGRAMAS</div></div>
-        <div id="boxSts"><section><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path fill="#965BA5" d="M25,49.794c-0.947,0-1.854-0.224-2.551-0.625L5.345,39.293c-1.454-0.839-2.551-2.738-2.551-4.419V15.125c0-1.679,1.097-3.578,2.551-4.418l17.104-9.875C23.146,0.428,24.053,0.206,25,0.206s1.854,0.222,2.552,0.625l17.104,9.875c1.453,0.84,2.551,2.74,2.551,4.418v19.749c0,1.681-1.098,3.58-2.551,4.419l-17.104,9.876C26.854,49.57,25.947,49.794,25,49.794zM25,2.492c-0.545,0-1.058,0.116-1.408,0.319L6.488,12.687c-0.737,0.425-1.409,1.587-1.409,2.438v19.749c0,0.852,0.671,2.015,1.409,2.44l17.103,9.875c0.35,0.202,0.863,0.318,1.408,0.318s1.058-0.116,1.407-0.318l17.105-9.875c0.737-0.426,1.408-1.589,1.408-2.44V15.125c0-0.851-0.671-2.014-1.408-2.438L26.408,2.811C26.058,2.608,25.545,2.492,25,2.492z"></path></svg><div id="qntSts">7</div></section><div id="ttlSts">PROJETOS</div></div>
+    	<div id="boxSts"><section><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path fill="#29B99A" d="M25,49.794c-0.947,0-1.854-0.224-2.551-0.625L5.345,39.293c-1.454-0.839-2.551-2.738-2.551-4.419V15.125c0-1.679,1.097-3.578,2.551-4.418l17.104-9.875C23.146,0.428,24.053,0.206,25,0.206s1.854,0.222,2.552,0.625l17.104,9.875c1.453,0.84,2.551,2.74,2.551,4.418v19.749c0,1.681-1.098,3.58-2.551,4.419l-17.104,9.876C26.854,49.57,25.947,49.794,25,49.794zM25,2.492c-0.545,0-1.058,0.116-1.408,0.319L6.488,12.687c-0.737,0.425-1.409,1.587-1.409,2.438v19.749c0,0.852,0.671,2.015,1.409,2.44l17.103,9.875c0.35,0.202,0.863,0.318,1.408,0.318s1.058-0.116,1.407-0.318l17.105-9.875c0.737-0.426,1.408-1.589,1.408-2.44V15.125c0-0.851-0.671-2.014-1.408-2.438L26.408,2.811C26.058,2.608,25.545,2.492,25,2.492z"></path></svg><div id="qntSts">0</div></section><div id="ttlSts">COPOS DE CAFE</div></div>
+        <div id="boxSts"><section><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path fill="#2BBCEE" d="M25,49.794c-0.947,0-1.854-0.224-2.551-0.625L5.345,39.293c-1.454-0.839-2.551-2.738-2.551-4.419V15.125c0-1.679,1.097-3.578,2.551-4.418l17.104-9.875C23.146,0.428,24.053,0.206,25,0.206s1.854,0.222,2.552,0.625l17.104,9.875c1.453,0.84,2.551,2.74,2.551,4.418v19.749c0,1.681-1.098,3.58-2.551,4.419l-17.104,9.876C26.854,49.57,25.947,49.794,25,49.794zM25,2.492c-0.545,0-1.058,0.116-1.408,0.319L6.488,12.687c-0.737,0.425-1.409,1.587-1.409,2.438v19.749c0,0.852,0.671,2.015,1.409,2.44l17.103,9.875c0.35,0.202,0.863,0.318,1.408,0.318s1.058-0.116,1.407-0.318l17.105-9.875c0.737-0.426,1.408-1.589,1.408-2.44V15.125c0-0.851-0.671-2.014-1.408-2.438L26.408,2.811C26.058,2.608,25.545,2.492,25,2.492z"></path></svg><div id="qntSts">2,9</div></section><div id="ttlSts" style="margin-top:70px;">MIL LINHAS PROGRAMAS</div></div>
+        <div id="boxSts"><section><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path fill="#965BA5" d="M25,49.794c-0.947,0-1.854-0.224-2.551-0.625L5.345,39.293c-1.454-0.839-2.551-2.738-2.551-4.419V15.125c0-1.679,1.097-3.578,2.551-4.418l17.104-9.875C23.146,0.428,24.053,0.206,25,0.206s1.854,0.222,2.552,0.625l17.104,9.875c1.453,0.84,2.551,2.74,2.551,4.418v19.749c0,1.681-1.098,3.58-2.551,4.419l-17.104,9.876C26.854,49.57,25.947,49.794,25,49.794zM25,2.492c-0.545,0-1.058,0.116-1.408,0.319L6.488,12.687c-0.737,0.425-1.409,1.587-1.409,2.438v19.749c0,0.852,0.671,2.015,1.409,2.44l17.103,9.875c0.35,0.202,0.863,0.318,1.408,0.318s1.058-0.116,1.407-0.318l17.105-9.875c0.737-0.426,1.408-1.589,1.408-2.44V15.125c0-0.851-0.671-2.014-1.408-2.438L26.408,2.811C26.058,2.608,25.545,2.492,25,2.492z"></path></svg><div id="qntSts">4</div></section><div id="ttlSts">PROJETOS</div></div>
         <div id="boxSts"><section><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path fill="#E7E7E7" d="M25,49.794c-0.947,0-1.854-0.224-2.551-0.625L5.345,39.293c-1.454-0.839-2.551-2.738-2.551-4.419V15.125c0-1.679,1.097-3.578,2.551-4.418l17.104-9.875C23.146,0.428,24.053,0.206,25,0.206s1.854,0.222,2.552,0.625l17.104,9.875c1.453,0.84,2.551,2.74,2.551,4.418v19.749c0,1.681-1.098,3.58-2.551,4.419l-17.104,9.876C26.854,49.57,25.947,49.794,25,49.794zM25,2.492c-0.545,0-1.058,0.116-1.408,0.319L6.488,12.687c-0.737,0.425-1.409,1.587-1.409,2.438v19.749c0,0.852,0.671,2.015,1.409,2.44l17.103,9.875c0.35,0.202,0.863,0.318,1.408,0.318s1.058-0.116,1.407-0.318l17.105-9.875c0.737-0.426,1.408-1.589,1.408-2.44V15.125c0-0.851-0.671-2.014-1.408-2.438L26.408,2.811C26.058,2.608,25.545,2.492,25,2.492z"></path></svg><div id="qntSts">2</div></section><div id="ttlSts">CLIENTES</div></div>
     </div>
 	<header id="perfil">
@@ -67,53 +69,98 @@
 </div>
 <div id="servicos">
 	<h1>Serviços</h1>
-    <div><img src="" title="" /><h2>DESIGN</h2><p></p></div>
-    <div><img src="" title="" /><h2>DEVELOPMENT</h2><p></p></div>
-    <div><img src="" title="" /><h2>CONSULTING</h2><p></p></div>
+    <div id="estruSvc">
+    	<img src="imgs/design-eae23e0c.svg" title="Design" />
+        <h2>Design</h2>
+        <ul>
+        	<li>Web, Desktop &amp; Mobile Design</li>
+        	<li>Melhor experiência</li>
+        	<li>Usabilidade</li>
+        	<li>Design de interfaces para usuários</li>
+        	<li>Arquitetura da informação</li>
+        	<li>Design Vídeos &amp; Renderização</li>
+        </ul>
+    </div>
+    <div id="estruSvc">
+    	<img src="imgs/development-491e9088.svg" title="Development" />
+        <h2>Desenvolvimento</h2>
+        <ul>
+        	<li>Websites</li>
+        	<li>Aplicações Web</li>
+        	<li>Sistemas Windows</li>
+        	<li>Aplicativos Android</li>
+        	<li>Gerenciamento de Conteúdo</li>
+        	<li>Sistema de E-Commerce</li>
+        </ul>
+    </div>
+    <div id="estruSvc">
+    	<img src="imgs/consulting-6e173135.svg" title="Consulting" />
+        <h2>Consultoria</h2>
+        <ul>
+        	<li>Integração com Mídia Social</li>
+        	<li>Search Marketing</li>
+        </ul>
+    </div>
 </div>
 <div id="empresas">
-	<img src="" title="" />
-    <img src="" title="" />
-    <img src="" title="" />
-    <img src="" title="" />
+	<span><img src="imgs/empresas/armazem.png" title="Armazem do Estudante" /></span>
+    <span><img src="imgs/empresas/comunicativa.png" title=" Comunicativa FM" /></span>
+	<span><img src="imgs/empresas/armazem.png" title="Armazem do Estudante" /></span>
+    <span><img src="imgs/empresas/comunicativa.png" title=" Comunicativa FM" /></span>
 </div>
 <div id="perfilCompleto">
-	<div id="fotoVideo"></div>
-	<div id="sobre"><h1></h1><p></p></div>
+	<div id="fotoVideo">
+    	<div clss="overlay"></div>
+        <video id="me_vid" preload="" autoplay="" loop="" muted="" height="380">
+            <source src="video/me3.mp4">
+            <source src="video/me3.webm">
+        </video>
+    </div>
+	<div id="sobre"><h1>Eu, Wellington A. A. Silverio</h1><p>Olá! Eu sou estudante da Escola Técnica Estadual (Centro Paula Souza), Hortolândia - SP, e eu trabalho atualmente como autônomo. Meu objetivo é trabalhar no setor de Analise de projetos WEB/MOBILE/SISTEMAS.</p></div>
 	<div id="informacoes">
-    	<section><img src="" title="" /></section>
-    	<section><img src="" title="" /></section>
-    	<section><img src="" title="" /></section>
-    	<section><img src="" title="" /></section>
-    	<section><img src="" title="" /></section>
-    	<section><img src="" title="" /></section>
+    	<section><i class="icon-doc-text-inv"></i>cv</section>
+    	<section><i class="icon-mail-1"></i>wellington.silverio@etec.sp.gov.br</section>
+    	<section><i class="icon-facebook"></i>facebook.com/Wellington.A.Silverio</section>
+    	<section><i class="icon-gplus"></i>plus.google.com/Wellington Silverio</section>
+    	<section><i class="icon-mobile"></i>+55 (19) 9 8317-2306</section>
+    	<section><i class="icon-github"></i>github.com/wellingtonsilverio</section>
     </div>
 </div>
 <div id="projetos">
-	<div>
-        <h1>titulo</h1>
-        <h2>O | QUE | FEZ</h2>
-        <img src="" title="" />
+	<div id="barraPjt" style="background-color:#424242;">
+    	<div id="contPjt">
+            <h1>titulo</h1>
+            <h2>O | QUE | FEZ</h2>
+            <a><div id="plus"></div></a>
+        </div>
     </div>
-	<div>
-        <h1>titulo</h1>
-        <h2>O | QUE | FEZ</h2>
-        <img src="" title="" />
+	<div id="barraPjt" style="background-color:#BD3638;">
+    	<div id="contPjt">
+            <h1>titulo</h1>
+            <h2>O | QUE | FEZ</h2>
+            <a><div id="plus"></div></a>
+        </div>
     </div>
-	<div>
-        <h1>titulo</h1>
-        <h2>O | QUE | FEZ</h2>
-        <img src="" title="" />
+	<div id="barraPjt" style="background-color:#174088;">
+    	<div id="contPjt">
+            <h1>titulo</h1>
+            <h2>O | QUE | FEZ</h2>
+            <a><div id="plus"></div></a>
+        </div>
     </div>
-	<div>
-        <h1>titulo</h1>
-        <h2>O | QUE | FEZ</h2>
-        <img src="" title="" />
+	<div id="barraPjt" style="background-color:#22965A;">
+    	<div id="contPjt">
+            <h1>titulo</h1>
+            <h2>O | QUE | FEZ</h2>
+            <a><div id="plus"></div></a>
+        </div>
     </div>
-	<div>
-        <h1>conheça mais</h1>
-        <h2>DIVERSOS</h2>
-        <img src="" title="" />
+	<div id="barraPjt" style="background-color:#773483;">
+    	<div id="contPjt">
+            <h1>conheça mais</h1>
+            <h2>DIVERSOS</h2>
+            <a><div id="plus"></div></a>
+        </div>
     </div>
 </div>
 <div id="opensource">
