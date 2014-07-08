26ce97035f35c36befb9d6a8939f8a88c512f483
Wellington A. A. Silverio <wellington.silverio@etec.sp.gov.br>
2014-07-08 11:22:34 -0300

    adicionado o arquivo de pedir projeto, com estrutura basica

---
+++ projeto.php
@@ -0,0 +1,56 @@
+<?php ?>
+<form>
+	<h1></h1>
+	<h2></h2>
+	<div>
+		<h3>Qual é o nome do Projeto?</h3>
+		<input type="text" placeholder="" id="" name="" />
+	</div>
+	<div>
+		<h3>Qual seu nome/nome da empresa onde trabalha?</h3>
+		<input type="text" placeholder="" id="" name="" />
+	</div>
+	<div>
+		<h3>Qual é seu e-mail empresarial ou de contato?</h3>
+		<input type="text" placeholder="" id="" name="" />
+	</div>
+	<div>
+		<h3>Qual é seu telefone de contato?</h3>
+		<input type="text" placeholder="" id="" name="" />
+	</div>
+	<div>
+		<h3>Qual é a melhor maneira, para você, de fazer contato?</h3>
+		<input type="checkbox" value="Telefone" />Telefone
+		<input type="checkbox" value="e-mail" />E-mail
+	</div>
+	<div>
+		<h3>Que tipo de projeto você pretende criar?</h3>
+		<input type="checkbox" value="Para tablets/smartphones" />Para tablets/smartphones
+		<input type="checkbox" value="Para Desktop(Maquina pessoal/empresarial)" />Para Desktop(Maquina pessoal/empresarial)
+		<input type="checkbox" value="Para Web" />Para Web
+	</div>
+	<div>
+		<h3>Qual o valor que você deseja investir?</h3>
+		<input type="checkbox" value="menos de R$500,00" />menos de R$500,00
+		<input type="checkbox" value="de R$500,00 a R$1.500,00" />de R$500,00 a R$1.500,00
+		<input type="checkbox" value="de R$1.500,00 a R$5.000,00" />de R$1.500,00 a R$5.000,00
+		<input type="checkbox" value="de R$5.000,00 a R$20.000,00" />de R$5.000,00 a R$20.000,00
+		<input type="checkbox" value="mais de R$20.000,00" />mais de R$20.000,00
+	</div>
+	<div>
+		<h3>Você precisa do projeto:</h3>
+		<select>
+			<option>Imediatamente</option>
+			<option>em um mes</option>
+			<option>em um semestre(6 meses)</option>
+			<option>em um ano (12 meses)</option>
+		</select>
+	</div>
+	<div>
+		<h3>Para você, o que é mais importante no projeto?</h3>
+		<input type="text" placeholder="" id="" name="" />
+	</div>
+	<div>
+		<input type="button" onclick="" value="Enviar Projeto" />
+	</div>
+</form>
