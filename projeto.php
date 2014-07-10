151b404a2ff6754d8f56f0d33dae6c20239fb261
Wellington A. A. Silverio <wellington.silverio@etec.sp.gov.br>
2014-07-09 23:52:27 -0300

    renovado o menu, adicionado no botao iniciar projeto o load ajax e mudado o fundo do projeto(top e footer)

--- projeto.php
+++ projeto.php
@@ -1,9 +1,10 @@
 <?php ?>
+<link href="css/projeto.css" rel="stylesheet" type="text/css" />
 <form>
 	<h1></h1>
 	<h2></h2>
 	<div>
-		<h3>Qual é o nome do Projeto?</h3>
+		<h3>Qual &eacute; o nome do Projeto?</h3>
 		<input type="text" placeholder="" id="" name="" />
 	</div>
 	<div>
@@ -11,26 +12,26 @@
 		<input type="text" placeholder="" id="" name="" />
 	</div>
 	<div>
-		<h3>Qual é seu e-mail empresarial ou de contato?</h3>
+		<h3>Qual &eacute; seu e-mail empresarial ou de contato?</h3>
 		<input type="text" placeholder="" id="" name="" />
 	</div>
 	<div>
-		<h3>Qual é seu telefone de contato?</h3>
+		<h3>Qual &eacute; seu telefone de contato?</h3>
 		<input type="text" placeholder="" id="" name="" />
 	</div>
 	<div>
-		<h3>Qual é a melhor maneira, para você, de fazer contato?</h3>
+		<h3>Qual &eacute; a melhor maneira, para voc&ecirc;, de fazer contato?</h3>
 		<input type="checkbox" value="Telefone" />Telefone
 		<input type="checkbox" value="e-mail" />E-mail
 	</div>
 	<div>
-		<h3>Que tipo de projeto você pretende criar?</h3>
+		<h3>Que tipo de projeto voc&ecirc; pretende criar?</h3>
 		<input type="checkbox" value="Para tablets/smartphones" />Para tablets/smartphones
 		<input type="checkbox" value="Para Desktop(Maquina pessoal/empresarial)" />Para Desktop(Maquina pessoal/empresarial)
 		<input type="checkbox" value="Para Web" />Para Web
 	</div>
 	<div>
-		<h3>Qual o valor que você deseja investir?</h3>
+		<h3>Qual o valor que voc&ecirc; deseja investir?</h3>
 		<input type="checkbox" value="menos de R$500,00" />menos de R$500,00
 		<input type="checkbox" value="de R$500,00 a R$1.500,00" />de R$500,00 a R$1.500,00
 		<input type="checkbox" value="de R$1.500,00 a R$5.000,00" />de R$1.500,00 a R$5.000,00
@@ -38,7 +39,7 @@
 		<input type="checkbox" value="mais de R$20.000,00" />mais de R$20.000,00
 	</div>
 	<div>
-		<h3>Você precisa do projeto:</h3>
+		<h3>Voc&ecirc; precisa do projeto:</h3>
 		<select>
 			<option>Imediatamente</option>
 			<option>em um mes</option>
@@ -47,10 +48,10 @@
 		</select>
 	</div>
 	<div>
-		<h3>Para você, o que é mais importante no projeto?</h3>
+		<h3>Para voc&ecirc;, o que &eacute; mais importante no projeto?</h3>
 		<input type="text" placeholder="" id="" name="" />
 	</div>
 	<div>
-		<input type="button" onclick="" value="Enviar Projeto" />
+		<input type="button" onclick="" value="Enviar Projeto" style="margin: 23px;" />
 	</div>
 </form>
